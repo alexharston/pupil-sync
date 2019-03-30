a9d885721e30a50859477d5b89e9b16a400a7ad9
Chaiyawan Auepanwiriyakul <ca1216@ic.ac.uk>
2019-03-30 10:21:07 +0000

    Update BackgroundTick.py

--- BackgroundTick.py
+++ BackgroundTick.py
@@ -1,49 +1,62 @@
 import nidaqmx
-import Task_Proxy
+from ctypes import c_bool
+from background_helper import Task_Proxy
+from time import sleep
 
 class BackgroundTick(Task_Proxy):
 
     def __init__(self, name, args=(), kwargs={'skipFirst': 0, 'skipFactor': 1}):
-        logger.info('Starting PyTick background thread')
-        super().__init__(name, None, args, kwargs)
+	
+        super().logger.info('Starting PyTick background thread')
+	self._should_terminate_flag = mp.Value(c_bool, 0)
+	
+        pipe_parent, pipe_child = mp.Pipe(True)
+        wrapper_args = super()._prepare_wrapper_args(
+            pipe_child, self._should_terminate_flag
+        )
+        wrapper_args.extend(args)
+        self.process = mp.Process(
+            target=self._wrapper, name=name, args=wrapper_args, kwargs=kwargs
+        )
+        self.process.daemon = True
+        self.process.start()
+        self.pipe = pipe_parent
+	
         niTask = nidaqmx.Task()
-		niTask.ao_channels.add_ao_voltage_chan('Dev1/ao1')
-		self.niTask = niTask
+	niTask.ao_channels.add_ao_voltage_chan('Dev1/ao1')
+	self.niTask = niTask
 
-    def _wrapper(self, pipe, _should_terminate_flag, generator, *args, **kwargs):
+    def _wrapper(self, pipe, _should_terminate_flag, *args, **kwargs):
         while True:
-
             if pipe.poll(0):
-
                 recvStr = pipe.recv()
                 if recvStr == 'trigger':
                     try:
-                        self.trigger(skipFirst, skipFactor)
+                        self.trigger(**kwargs)
                     except Exception as e:
                         pipe.send(e)
                         if not isinstance(e, EarlyCancellationError):
                             import traceback
-
-                            logger.info(traceback.format_exc())
+                            super().logger.info(traceback.format_exc())
                         break
                 
                 elif recvStr == 'stopped':
                     break
-
                 else:
-                    logger.info('Invalid trigger received')
+                    super().logger.info('Invalid trigger received')
         
         pipe.close()
-        logger.debug("Exiting _wrapper")
+        super().logger.debug("Exiting _wrapper")
 
     def fetch(self):
         print('En Taro Tassadar!')
 
-    def trigger(self, skipFirst, skipFactor):
-        for _ in range(skipFirst + skipFactor):
+    def trigger(self, skipFirst=0, skipFactor=1):
+        for _ in range(skipFirst + skipFactor + 1):
             self.niTask.write([3.3], auto_start=True)
-            time.sleep(0.002)
+            sleep(0.002)
             self.niTask.write([0.0], auto_start=True)
+	    sleep(0.001)
 
     def finish(self, timeout=1):
         self.niTask.stop()
