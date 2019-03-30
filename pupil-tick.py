13025a80f617dc524b28175c54b1a23c3b8326ec
Chaiyawan Auepanwiriyakul <ca1216@ic.ac.uk>
2019-03-30 10:37:58 +0000

    Add prototype UI

--- pupil-tick.py
+++ pupil-tick.py
@@ -1,7 +1,6 @@
 from BackgroundTick import BackgroundTick
 from plugin import Plugin
 from pyglui import ui
-from time import time
 import os
 import sys
 import logging
@@ -11,40 +10,56 @@ class PyTick(Plugin):
 	icon_chr = "'"
 	icon_font="roboto"
 
-	def __init__(self, g_pool):
+	def __init__(self, g_pool, skipFactor=1, skipFirst=0):
 		super().__init__(g_pool)
 
 		self.uniqueness = "by_class"
 		self.order = 1
 
 		self.state = 'stopped'
-        self.proxy = CustomTaskProxy('BackgroundTick')
+        	self.proxy = CustomTaskProxy(
+			'BackgroundTick', kwargs={'skipFactor': skipFactor, 'skipFirst':skipFirst}
+		)
+		self.skipFactor = skipFactor
+		self.skipFirst = skipFirst
 
 	def init_ui(self):
 		self.add_menu()
 		self.menu.label = 'PyTick'
 		help_str = "PyTick"
 		self.menu.append(ui.Info_Text(help_str))
+		self.menu.append(ui.Text_Input('skipFactor'), self, label='Skip Factor')
+		self.menu.append(ui.Text_Input('skipFirst'), self, label='Skip First')
 
 	def deinit_ui(self):
 		self.remove_menu()
 
 
 	def get_init_dict(self):
-		return {}
+		return {'skipFactor': self.skipFactor,
+			'skipFirst': self.skipFirst}
+	
+	def on_char(self, character):
+		if character is 's':
+			if self.state == 'stopped':
+				self.state = 'started'
+				self.proxy.pipe.send('trigger')
+			else self.state == 'started':
+				self.state = 'stopped'
+				self.proxy.pipe.send('trigger')
 
 	def on_notify(self, notification):
-        # print(notification['subject'])
-        if notification['subject'] == 'recording.should_start' and self.state == 'stopped':
-                self.state = 'started'
-                self.proxy.pipe.send('trigger')
-        elif notification['subject'] == 'recording.should_stop' and self.state == 'started':
-                self.state = 'stopped'
-                self.proxy.pipe.send('trigger')
-        else:
-                print('Are you nut?!!!')   
+		# print(notification['subject'])
+		if notification['subject'] == 'recording.should_start' and self.state == 'stopped':
+			self.state = 'started'
+			self.proxy.pipe.send('trigger')
+		elif notification['subject'] == 'recording.should_stop' and self.state == 'started':
+			self.state = 'stopped'
+			self.proxy.pipe.send('trigger')
+		else:
+			print('Are you nut?!!!')
 
-    def cleanup(self):
-        self.proxy.pipe.send('stopped')
-        self.proxy.finish()
+    	def cleanup(self):
+        	self.proxy.pipe.send('stopped')
+        	self.proxy.finish()
 
