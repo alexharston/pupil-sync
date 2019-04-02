f1ad04023464bed9f9feaf49786d742fc5da9312
hl6114 <CA1216@ic.ac.uk>
2019-04-02 13:18:01 +0100

    changed files

--- BackgroundTick.py
+++ BackgroundTick.py
@@ -1,65 +1,73 @@
 import nidaqmx
 from ctypes import c_bool
-from background_helper import Task_Proxy
+from background_helper import Task_Proxy, EarlyCancellationError
 from time import sleep
+import multiprocessing as mp
 
 class BackgroundTick(Task_Proxy):
 
-    def __init__(self, name, args=(), kwargs={}):
-	
-        super().logger.info('Starting PyTick background thread')
-	self._should_terminate_flag = mp.Value(c_bool, 0)
-	
+    def __init__(self, name, args=[], kwargs={}):
+    
+        # super().logger.info('Starting PyTick background thread')
+        self._should_terminate_flag = mp.Value(c_bool, 0)
+        
         pipe_parent, pipe_child = mp.Pipe(True)
         wrapper_args = super()._prepare_wrapper_args(
             pipe_child, self._should_terminate_flag
         )
-        wrapper_args.extend(args)
+        # wrapper_args.extend(args)
         self.process = mp.Process(
             target=self._wrapper, name=name, args=wrapper_args, kwargs=kwargs
         )
         self.process.daemon = True
         self.process.start()
         self.pipe = pipe_parent
-	
-        niTask = nidaqmx.Task()
-	niTask.ao_channels.add_ao_voltage_chan('Dev1/ao1')
-	self.niTask = niTask
+
+       
 
     def _wrapper(self, pipe, _should_terminate_flag, *args, **kwargs):
+
+        niTask = nidaqmx.Task()
+        niTask.ao_channels.add_ao_voltage_chan('Dev1/ao1')
+
+        def trigger(skipFirst=0, skipFactor=1):
+            for _ in range(skipFirst + skipFactor + 1):
+                niTask.write([3.3], auto_start=True)
+                sleep(0.002)
+                niTask.write([0.0], auto_start=True)
+                sleep(0.001)
+
         while True:
             if pipe.poll(0):
                 recvStr = pipe.recv()
                 if recvStr == 'trigger':
                     try:
-                        self.trigger(**kwargs)
+                        trigger(**kwargs)
                     except Exception as e:
                         pipe.send(e)
                         if not isinstance(e, EarlyCancellationError):
                             import traceback
-                            super().logger.info(traceback.format_exc())
+                            # super().logger.info(traceback.format_exc())
+                            pass
                         break
                 
                 elif recvStr == 'stopped':
                     break
                 else:
-                    super().logger.info('Invalid trigger received')
+                    # super().logger.info('Invalid trigger received')
+                    pass
         
         pipe.close()
-        super().logger.debug("Exiting _wrapper")
+        niTask.stop()
+        print('NI Task Stopped')
+        # super().logger.debug("Exiting _wrapper")
 
     def fetch(self):
-        print('En Taro Tassadar!')
+        print('Fetched')
 
-    def trigger(self, skipFirst=0, skipFactor=1):
-        for _ in range(skipFirst + skipFactor + 1):
-            self.niTask.write([3.3], auto_start=True)
-            sleep(0.002)
-            self.niTask.write([0.0], auto_start=True)
-	    sleep(0.001)
+ 
 
     def finish(self, timeout=1):
-        self.niTask.stop()
         if self.process is not None:
             self.process.join(timeout)
             self.process = None
--- pupil-tick.py
+++ pupil-tick.py
@@ -7,59 +7,57 @@ import logging
 #import NI module
 
 class PyTick(Plugin):
-	icon_chr = "'"
-	icon_font="roboto"
+    icon_chr = "'"
+    icon_font="roboto"
 
-	def __init__(self, g_pool, skipFactor=1, skipFirst=0):
-		super().__init__(g_pool)
+    def __init__(self, g_pool, skipFactor=1, skipFirst=0):
+        super().__init__(g_pool)
 
-		self.uniqueness = "by_class"
-		self.order = 1
+        self.uniqueness = "by_class"
+        self.order = 1
 
-		self.state = 'stopped'
-        	self.proxy = CustomTaskProxy(
-			'BackgroundTick', kwargs={'skipFactor': skipFactor, 'skipFirst':skipFirst}
-		)
-		self.skipFactor = skipFactor
-		self.skipFirst = skipFirst
+        self.state = 'stopped'
+        self.proxy = BackgroundTick(
+        'BackgroundTick', kwargs={'skipFactor': skipFactor, 'skipFirst':skipFirst})
+        self.skipFactor = skipFactor
+        self.skipFirst = skipFirst
 
-	def init_ui(self):
-		self.add_menu()
-		self.menu.label = 'PyTick'
-		help_str = "PyTick"
-		self.menu.append(ui.Info_Text(help_str))
-		self.menu.append(ui.Text_Input('skipFactor'), self, label='Skip Factor')
-		self.menu.append(ui.Text_Input('skipFirst'), self, label='Skip First')
+    def init_ui(self):
+        self.add_menu()
+        self.menu.label = 'PyTick'
+        help_str = "PyTick"
+        self.menu.append(ui.Info_Text(help_str))
+        # self.menu.append(ui.Text_Input('skipFactor'), self, label='Skip Factor')
+        # self.menu.append(ui.Text_Input('skipFirst'), self, label='Skip First')
 
-	def deinit_ui(self):
-		self.remove_menu()
+    def deinit_ui(self):
+        self.remove_menu()
 
 
-	def get_init_dict(self):
-		return {'skipFactor': self.skipFactor,
-			'skipFirst': self.skipFirst}
-	
-	def on_char(self, character):
-		if character is 's':
-			if self.state == 'stopped':
-				self.state = 'started'
-				self.proxy.pipe.send('trigger')
-			else self.state == 'started':
-				self.state = 'stopped'
-				self.proxy.pipe.send('trigger')
+    def get_init_dict(self):
+        return {'skipFactor': self.skipFactor, 'skipFirst': self.skipFirst}
+    
+    def on_char(self, character):
+        if character is 's':
+            if self.state == 'stopped':
+                self.state = 'started'
+                self.proxy.pipe.send('trigger')
+            else:
+                self.state = 'stopped'
+                self.proxy.pipe.send('trigger')
 
-	def on_notify(self, notification):
-		# print(notification['subject'])
-		if notification['subject'] == 'recording.should_start' and self.state == 'stopped':
-			self.state = 'started'
-			self.proxy.pipe.send('trigger')
-		elif notification['subject'] == 'recording.should_stop' and self.state == 'started':
-			self.state = 'stopped'
-			self.proxy.pipe.send('trigger')
-		else:
-			print('Are you nut?!!!')
+    def on_notify(self, notification):
+        # print(notification['subject'])
+        if (notification['subject'] == 'recording.should_start') and self.state == 'stopped':
+            self.state = 'started'
+            self.proxy.pipe.send('trigger')
+        elif notification['subject'] == 'recording.should_stop' and self.state == 'started':
+            self.state = 'stopped'
+            self.proxy.pipe.send('trigger')
+        else:
+            print(notification['subject'])
 
-    	def cleanup(self):
-        	self.proxy.pipe.send('stopped')
-        	self.proxy.finish()
+        def cleanup(self):
+            self.proxy.pipe.send('stopped')
+            self.proxy.finish()
 
